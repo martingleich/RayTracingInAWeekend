2640df2f052fba0f7365cc9cb72c707535f06156
Martin Gleich <martin.gleich3@web.de>
2022-10-14 12:35:40 +0200

    display normal of interaction

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,24 @@ fn lerp<T: std::ops::Add<Output = T> + std::ops::Mul<f32, Output = T>>(a: T, b: 
     a * (1.0 - t) + b * t
 }
 
-fn hit_sphere(center: Point3, radius: f32, ray: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f32, ray: Ray) -> f32 {
     let oc = ray.origin - center;
     let a = ray.direction.length_squared();
-    let b = Dir3::dot(oc, ray.direction);
+    let half_b = Dir3::dot(oc, ray.direction);
     let c = oc.length_squared() - radius * radius;
-    b * b > a * c
+    let disc = half_b*half_b - a*c;
+    if disc < 0.0 {
+        return -1.0;
+    } else {
+        return (-half_b - disc.sqrt()) / a;
+    }
 }
 
 fn ray_color(ray: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Color::new_rgb(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let n = (ray.at(t) - Point3::new(0.0, 0.0, -1.0)).unit();
+        return 0.5 * Color::new_rgb(n.x+1.0, n.y+1.0, n.z+1.0);
     }
     let unit_direction = ray.direction.unit();
     let t = 0.5 * (Dir3::dot(Dir3::UNIT_Y, unit_direction) + 1.0);
