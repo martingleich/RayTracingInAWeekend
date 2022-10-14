35bedd2486c1c1a98288f0c4ef0bc69b92b91002
Martin Gleich <martin.gleich3@web.de>
2022-10-14 11:40:29 +0200

    add ray type

---
+++ src/ray.rs
@@ -0,0 +1,18 @@
+use crate::vec3::Dir3;
+use crate::vec3::Point3;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct Ray
+{
+    origin: Point3,
+    direction : Dir3,
+}
+
+impl Ray
+{
+    pub fn at(self, t:f32) -> Point3
+    {
+        self.origin + t*self.direction
+    }
+}
+
