64b2ccc3716cf031d7f9c51139e5e55ebf4e5871
Martin Gleich <martin.gleich3@web.de>
2022-10-14 12:16:53 +0200

    fix formatting in ray

--- src/ray.rs
+++ src/ray.rs
@@ -2,17 +2,13 @@ use crate::vec3::Dir3;
 use crate::vec3::Point3;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
-pub struct Ray
-{
-    origin: Point3,
-    direction : Dir3,
+pub struct Ray {
+    pub origin: Point3,
+    pub direction: Dir3,
 }
 
-impl Ray
-{
-    pub fn at(self, t:f32) -> Point3
-    {
-        self.origin + t*self.direction
+impl Ray {
+    pub fn at(self, t: f32) -> Point3 {
+        self.origin + t * self.direction
     }
 }
-
