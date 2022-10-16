bdd840c21969afb07489fb8ba200955bd2d7b6a8
Martin Gleich <martin.gleich3@web.de>
2022-10-16 20:20:59 +0200

    cleanup hittable

--- src/hittable.rs
+++ src/hittable.rs
@@ -46,38 +46,34 @@ impl Sphere {
 
 impl Hittable for Sphere {
     fn hit(&self, ray: Ray, t_min: f32, t_max: f32) -> Option<HitInteraction> {
-        let oc = ray.origin - self.center;
-        let a = ray.direction.length_squared();
-        let half_b = Dir3::dot(oc, ray.direction);
-        let c = oc.length_squared() - self.radius * self.radius;
-
-        let disc = half_b * half_b - a * c;
-        if disc < 0.0 {
-            return None;
-        } else {
-            let sqrtd = disc.sqrt();
-
-            let root_small = (-half_b - sqrtd) / a;
-            let t = if root_small < t_min || root_small > t_max {
-                let root_large = (-half_b + sqrtd) / a;
-                if root_large < t_min || root_large > t_max {
-                    return None;
-                } else {
-                    root_large
-                }
+        let maybe_t: Option<f32> = {
+            let oc = ray.origin - self.center;
+            let a = ray.direction.length_squared();
+            let half_b = Dir3::dot(oc, ray.direction);
+            let c = oc.length_squared() - self.radius * self.radius;
+            let disc = half_b * half_b - a * c;
+            if disc < 0.0 {
+                None
             } else {
-                root_small
-            };
-
+                let sqrtd = disc.sqrt();
+                let root_small = (-half_b - sqrtd) / a;
+                if root_small < t_min || root_small > t_max {
+                    let root_large = (-half_b + sqrtd) / a;
+                    if root_large < t_min || root_large > t_max {
+                        None
+                    } else {
+                        Some(root_large)
+                    }
+                } else {
+                    Some(root_small)
+                }
+            }
+        };
+        maybe_t.map(|t| {
             let position = ray.at(t);
             let surface_normal = (position - self.center) / self.radius;
-            return Some(HitInteraction::new_from_ray(
-                ray,
-                position,
-                surface_normal,
-                t,
-            ));
-        }
+            HitInteraction::new_from_ray(ray, position, surface_normal, t)
+        })
     }
 }
 
